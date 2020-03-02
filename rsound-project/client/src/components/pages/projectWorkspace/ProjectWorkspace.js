26c60b7293bfb6ee616e027015ac427497c4016b
Roberto Marrero <robertomarrerop@hotmail.com>
2020-03-02 13:30:41 +0100

    making the notes

--- rsound-project/client/src/components/pages/projectWorkspace/ProjectWorkspace.js
+++ rsound-project/client/src/components/pages/projectWorkspace/ProjectWorkspace.js
@@ -15,8 +15,27 @@ class Project extends Component {
     render() {
         return (
             <>
-                <Button variant="dark" type="submit" onClick={startOsc()} frequency="140">Play</Button>
-                <Button variant="dark" type="submit" onClick={stopOsc()} >Stop</Button>
+                <Button variant="dark" type="submit" onClick={() => startOsc(21.83)}>F0</Button>
+                <Button variant="dark" type="submit" onClick={() => startOsc(23.12)}>F#0</Button>
+                <Button variant="dark" type="submit" onClick={() => startOsc(24.50)}>G0</Button>
+                <Button variant="dark" type="submit" onClick={() => startOsc(25.96)}>G#0</Button>
+                <Button variant="dark" type="submit" onClick={() => startOsc(27.50)}>A0</Button>
+                <Button variant="dark" type="submit" onClick={() => startOsc(29.14)}>A#0</Button>
+                <Button variant="dark" type="submit" onClick={() => startOsc(30.87)}>B0</Button>
+                <Button variant="dark" type="submit" onClick={() => startOsc(32.70)}>C1</Button>
+                <Button variant="dark" type="submit" onClick={() => startOsc(34.65)}>C#1</Button>
+                <Button variant="dark" type="submit" onClick={() => startOsc(36.71)}>D1</Button>
+                <Button variant="dark" type="submit" onClick={() => startOsc(38.89)}>D#1</Button>
+                <Button variant="dark" type="submit" onClick={() => startOsc(23.12)}>F#0</Button>
+                <Button variant="dark" type="submit" onClick={() => startOsc(23.12)}>F#0</Button>
+                <Button variant="dark" type="submit" onClick={() => startOsc(23.12)}>F#0</Button>
+                <Button variant="dark" type="submit" onClick={() => startOsc(23.12)}>F#0</Button>
+                <Button variant="dark" type="submit" onClick={() => startOsc(23.12)}>F#0</Button>
+                <Button variant="dark" type="submit" onClick={() => startOsc(23.12)}>F#0</Button>
+                <Button variant="dark" type="submit" onClick={() => startOsc(23.12)}>F#0</Button>
+
+
+                <Button variant="dark" type="submit" onClick={() => stopOsc()} >Stop</Button>
             </>
         )
     }
--- rsound-project/client/src/components/pages/projectWorkspace/Synth.js
+++ rsound-project/client/src/components/pages/projectWorkspace/Synth.js
@@ -1,34 +1,26 @@
 
 let audioCtx = new window.AudioContext()
-
-let lfo = audioCtx.createOscillator()
-let vca = audioCtx.createGain()
 let osc = audioCtx.createOscillator()
 let gain = audioCtx.createGain()
 
 export const startOsc = (freq) => {
-    gain.gain.value = 1
-    osc.detune.value = 0
-    lfo.connect(vca.gain)
-    osc.connect(vca)
-    vca.connect(audioCtx.destination)
+    //stopOsc()
+    // Frequency is passed to this function from input button 
 
-    lfo.frequency.value = 4
-
-    // lfo.start(0)
-
-    osc = audioCtx.createOscillator() // Create sound source
-    osc.type = 0 // Sine wave
-    osc.frequency.value = 230 // Frequency in hertz (passed from input button)
-    osc.start(0) // Play oscillator instantly
+    // Create OscillatorNode
+    osc = audioCtx.createOscillator(); // Create sound source
+    osc.type = 'sine' // Sine wave
+    osc.frequency.value = freq; // Frequency in hertz (passed from input button)
+    osc.start(0); // Play oscillator instantly
 
     // Create GainNode	
-    gain = audioCtx.createGain() // Create gain node
-    gain.gain.value = 1 // Set gain to full volume
+    gain = audioCtx.createGain(); // Create gain node
+    gain.gain.value = 1; // Set gain to full volume
 
     // Connect the Nodes
-    osc.connect(gain) // Connect oscillator to gain
-    gain.connect(audioCtx.destination) // Connect gain to output
+    osc.connect(gain); // Connect oscillator to gain
+    gain.connect(audioCtx.destination); // Connect gain to output
+
 }
 
 export const stopOsc = () => {
