70d5cbef9dc84d14eef1cf91ee78c99cfa05be00
Roberto Marrero <robertomarrerop@hotmail.com>
2020-03-02 15:21:11 +0100

    creatig waves

--- rsound-project/client/src/components/pages/projectWorkspace/Synth.js
+++ rsound-project/client/src/components/pages/projectWorkspace/Synth.js
@@ -2,24 +2,36 @@
 let audioCtx = new window.AudioContext()
 let osc = audioCtx.createOscillator()
 let gain = audioCtx.createGain()
+let wave = audioCtx.createPeriodicWave(wavetable.real, wavetable.imag)
 
 export const startOsc = (freq) => {
-    //stopOsc()
+
+
+    let osc = audioCtx.createOscillator();
+    osc.setPeriodicWave(wave);
+    osc.frequency.value = freq;
+    osc.connect(audioCtx.destination);
+    osc.start();
+    osc.stop(audioCtx.currentTime + 1);
+
+
+
+    //////////////////////////////////////////////////////////
     // Frequency is passed to this function from input button 
 
     // Create OscillatorNode
-    osc = audioCtx.createOscillator(); // Create sound source
-    osc.type = 'sine' // Sine wave
-    osc.frequency.value = freq; // Frequency in hertz (passed from input button)
-    osc.start(0); // Play oscillator instantly
+    //osc = audioCtx.createOscillator(); // Create sound source
+    // osc.type = 'sine' // Sine wave
+    // osc.frequency.value = freq; // Frequency in hertz (passed from input button)
+    // osc.start(0); // Play oscillator instantly
 
     // Create GainNode	
-    gain = audioCtx.createGain(); // Create gain node
-    gain.gain.value = 1; // Set gain to full volume
+    // gain = audioCtx.createGain(); // Create gain node
+    // gain.gain.value = 1; // Set gain to full volume
 
-    // Connect the Nodes
-    osc.connect(gain); // Connect oscillator to gain
-    gain.connect(audioCtx.destination); // Connect gain to output
+    // // Connect the Nodes
+    // osc.connect(gain); // Connect oscillator to gain
+    // gain.connect(audioCtx.destination); // Connect gain to output
 
 }
 
