c51705c90ea4219fa38985a4878adbf318554565
Roberto Marrero <robertomarrerop@hotmail.com>
2020-03-05 15:00:53 +0100

    deploy6

--- client/src/services/files.services.js
+++ client/src/services/files.services.js
@@ -4,7 +4,7 @@ export default class Services {
 
     constructor() {
         this.service = axios.create({
-            baseURL: `${process.env.REACT_APP_URL}/auth`,
+            baseURL: `${process.env.REACT_APP_URL}/files`,
             withCredentials: true   // RUTAS PERSISTENTES
         })
     }
